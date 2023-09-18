ad230b8c80adb422a22b8942a38e65e33864110c
Developer <136099805+Developer7861@users.noreply.github.com>
2023-09-18 20:01:25 +0530

    Create tomcatDeploy.groovy

---
+++ vars/tomcatDeploy.groovy
@@ -0,0 +1,10 @@
+def call(ip,creds,warFile){
+  // install ssh agent plugin
+    sshagent([creds]) {
+     // copy war file local to tomcat -dev server
+     sh "scp -o StrictHostKeyChecking=no target/${warFile} ec2-user@${ip}:/opt/tomcat9/webapps"
+     // restart the tomcat9 server
+    sh "ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"\
+    sh "ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
+  }
+}
