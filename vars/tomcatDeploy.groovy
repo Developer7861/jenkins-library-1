3670561b89b3a3d7730408db99bee4f504e9045a
Developer <136099805+Developer7861@users.noreply.github.com>
2023-09-19 12:58:54 +0530

    Update tomcatDeploy.groovy

--- vars/tomcatDeploy.groovy
+++ vars/tomcatDeploy.groovy
@@ -4,7 +4,7 @@ def call(ip,creds,warFile){
      // copy war file local to tomcat -dev server
      sh "scp -o StrictHostKeyChecking=no target/${warFile} ec2-user@${ip}:/opt/tomcat9/webapps"
      // restart the tomcat9 server
-    sh "ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"\
+    sh "ssh ec2-user@${ip} /opt/tomcat9/bin/shutdown.sh"
     sh "ssh ec2-user@${ip} /opt/tomcat9/bin/startup.sh"
   }
 }
